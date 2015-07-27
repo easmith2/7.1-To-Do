6686e734f70b15ef67ba58c955977cc7a99d9cf6
Elizabeth Smith <liz.allen.smith@gmail.com>
2015-07-27 17:07:48 -0400

    basic functionality, without removal of items

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 ;(function() {
+  var stoopidForm = document.querySelector('[data-js="myStoopidForm"]');
+  var taskEntered = document.querySelector('[data-js="taskEntry"]');
+  var taskList = document.querySelector('[data-js="todoItems"]');
 
+  stoopidForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+  });
+
+  taskEntered.addEventListener('keyup', function(e) {
+    var key = e.which || e.keyCode; // might need `|| 0` at the end of this
+    if(key === 13) {
+      if(taskEntered.value.trim().length === 0) {
+        alert('The force is weak with you. Try again.');
+      } else {
+        var defaultLi = document.querySelector('[data-js="defaultLi"]');
+        if(defaultLi) {
+          taskList.removeChild(defaultLi);
+        }
+        var node = document.createElement("LI");
+        console.log(node);
+        var removeButton = "<button class='todo__itemRemove'>&#x2717;</button>";
+        node.classList.add('todo__item');
+        node.innerHTML = "<input type='checkbox' /><label>" + taskEntered.value + "</label>" + removeButton;
+        taskList.appendChild(node);
+      }
+      stoopidForm.reset();
+    }
+  });
 })()
-
-
-
-//<li class="todo__item">
-//  <input type="checkbox" id="c1" />
-//  <label for="c1">Curse Rebel Scum</label>
-//  <button class="todo__itemRemove">&#x2717;</button>
-//</li>
